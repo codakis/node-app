c8dfed2533281c7d00e32151e6f2137af6ce5ce9
Akis Kontakis <akis@inked.gr>
2020-07-12 16:13:54 +0300

    arrow functions examples etc

---
+++ Play.js
@@ -0,0 +1,16 @@
+var name = "Akis";
+var age = 38;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobbies) {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHasHobbies
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
---
+++ play-extended.js
@@ -0,0 +1,22 @@
+const name = "Akis";
+let age = 38;
+const hasHobbies = true;
+
+age = 30;
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHasHobby
+  );
+};
+//arrow functions example
+const add = (a, b) => a + b;
+const addOne = (a) => a + 1;
+const addRandom = () => 1 + 2;
+
+console.log(summarizeUser(name, age, hasHobbies));
